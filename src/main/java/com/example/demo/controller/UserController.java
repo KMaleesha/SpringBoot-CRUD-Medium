2d06d979f9355a94af74ca9eab187b9ff6206721
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-08-11 15:46:02 +0530

    initial commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "api/v1/")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getusers")
+    public List<UserDTO> getUser() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/saveuser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO) {
+        return userService.saveUser(userDTO);
+    }
+
+    @PutMapping("/updateuser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO) {
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("deleteuser/{userId}")
+    public String deleteUser(@PathVariable int userId) {
+        return userService.deleteUser(userId);
+    }
+
+    @GetMapping("getuser/{userId}")
+    public UserDTO getUserById(@PathVariable int userId) {
+        return userService.getUser(userId);
+    }
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,52 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.User;
+import com.example.demo.repo.UserRepo;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    public List<UserDTO> getAllUsers() {
+        List<User> userList = userRepository.findAll();
+        return modelMapper.map(userList, new TypeToken<List<UserDTO>>() {}.getType());
+    }
+
+    public UserDTO saveUser(UserDTO userDTO) {
+        userRepo.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+
+    public UserDTO updateUser(UserDTO userDTO) {
+        userRepo.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+
+    public String deleteUser(int userId) {
+//        userRepo.delete(modelMapper.map(userDTO, User.class));
+        userRepo.deleteById((userId));
+        return "User deleted";
+    }
+
+    public UserDTO getUser(int userId) {
+        User user = userRepo.getUsersById(userId);
+        return modelMapper.map(user, UserDTO.class);
+    }
+}
